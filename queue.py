45c9dd0d144f5a226a5769adffed89400722eb7d
rekha <rekhagitcse@gmail.com>
2014-02-01 14:16:30 +0530

    Update queue.py

--- queue.py
+++ queue.py
@@ -1,4 +1,4 @@
-class Queue:
+class queue:
     def __init__(self):
         self.items = []
 
@@ -13,3 +13,45 @@ class Queue:
 
     def size(self):
         return len(self.items)
+        
+        
+        
+q=Queue()
+q.isEmpty()
+
+q.enqueue('python')
+q.enqueue(5)
+q=Queue()
+q.isEmpty()
+
+q.enqueue(87)
+q.enqueue('git')
+q.enqueue(True)
+
+
+
+#hotpotato game importing above class 'queue'
+
+import queue
+
+def hotPotato(namelist, num):
+    simqueue = Queue()
+    for name in namelist:
+        simqueue.enqueue(name)
+
+    while simqueue.size() > 1:
+        for i in range(num):
+            simqueue.enqueue(simqueue.dequeue())
+
+        simqueue.dequeue()
+
+    return simqueue.dequeue()
+
+print(hotPotato(["a","b","c","x","y","z"],7))
+
+
+#it returns 'c' as output
+
+
+
+
