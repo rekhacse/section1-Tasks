9eb66f4fe121919defdb9a1c3cb262b91ce3a34a
rekha <rekhagitcse@gmail.com>
2014-02-01 14:22:12 +0530

    Create graph.py

---
+++ graph.py
@@ -0,0 +1,66 @@
+ graph = {'A': ['B', 'C'],
+             'B': ['C', 'D'],
+             'C': ['D'],
+             'D': ['C'],
+             'E': ['F'],
+             'F': ['C']}
+             
+             
+def find_path(graph, start, end, path=[]):
+        path = path + [start]
+        if start == end:
+            return path
+        if not graph.has_key(start):
+            return None
+        for node in graph[start]:
+            if node not in path:
+                newpath = find_path(graph, node, end, path)
+                if newpath: return newpath
+        return None
+        
+        
+# >>> find_path(graph, 'A', 'D')
+    ['A', 'B', 'C', 'D']
+    
+    
+def find_all_paths(graph, start, end, path=[]):
+        path = path + [start]
+        if start == end:
+            return [path]
+        if not graph.has_key(start):
+            return []
+        paths = []
+        for node in graph[start]:
+            if node not in path:
+                newpaths = find_all_paths(graph, node, end, path)
+                for newpath in newpaths:
+                    paths.append(newpath)
+        return paths
+        
+        
+        
+#>>> find_all_paths(graph, 'A', 'D')
+    [['A', 'B', 'C', 'D'], ['A', 'B', 'D'], ['A', 'C', 'D']]
+    
+    
+    
+def find_shortest_path(graph, start, end, path=[]):
+        path = path + [start]
+        if start == end:
+            return path
+        if not graph.has_key(start):
+            return None
+        shortest = None
+        for node in graph[start]:
+            if node not in path:
+                newpath = find_shortest_path(graph, node, end, path)
+                if newpath:
+                    if not shortest or len(newpath) < len(shortest):
+                        shortest = newpath
+        return shortest
+        
+        
+#>>> find_shortest_path(graph, 'A', 'D')
+    ['A', 'C', 'D']
+        
+        
